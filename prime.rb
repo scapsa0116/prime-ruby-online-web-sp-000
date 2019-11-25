8fefbae4f9bc7a8d5321c5168de6d2a6d5ec6a1d
Stela Capsa <stela.capsa@gmail.com>
2019-11-25 19:35:08 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime?(n)
 return false if !n.integer?
 return false if n < 2
 end return true if n == 2
- (2..n-1).each (|int| return false if n % int == 0)
+ (2..n-1).each {|int| return false if n % int == 0}
  true
  end
 
